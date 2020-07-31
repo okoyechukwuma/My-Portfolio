8b93f5b19e6c5ee060044e3c178498092bf61de4
Chukwuma <okoyechukwuma310@gmail.com>
2020-07-31 03:46:46 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,19 +1,13 @@
 
-const home = document.querySelectorAll(".nav-link");
-const lightText = document.querySelectorAll(".text-bright");
 const contact = document.querySelector("#contact");
-const dark = document.querySelectorAll(".dark-background");
 const shadowRemove = document.querySelectorAll(".shadow-remove");
-const blackBackground = document.querySelectorAll(".black-color");
 const addContact = document.querySelector(".contact");
 const removeContact = document.querySelector(".contact-remove");
 const addMenu = document.querySelector(".menu-add");
 const addMainMenu = document.querySelector(".mainnavbar");
 const addMenuContent = document.querySelector(".mainnavbar-content");
 const removeMenu = document.querySelector(".menu-remove");
-const toggleBack = document.querySelector(".toggle-round");
-const mainToggler = document.querySelector(".toggle-box");
-const body = document.getElementById("body");
+
 
 
 
@@ -48,29 +42,31 @@ removeMenu.addEventListener('click', () => {
         });
     }
 
-    toggleBack.addEventListener('click', () => {
-        toggleBack.classList.toggle("push");
-        toggleBack.classList.toggle("toggle-dark-round");
-        mainToggler.classList.toggle("toggle-dark");
-        Array.from(dark).map((darktext) => {
-            darktext.classList.toggle("dark");
-        });
-        Array.from(lightText).map((lightText) => {
-            lightText.classList.toggle("light");
-        });
-        Array.from(shadowRemove).map((shadowCut) => {
-            shadowCut.classList.toggle("shadow");
-        });
-        Array.from(blackBackground).map((blakBac) => {
-            blakBac.classList.toggle("black");
-        });
-            let homeLen, i;
-            homeLen = home.length;
-        for (i = 0;i < homeLen; i++) {
-            const element = home[i];
-            element.classList.toggle("light");
-        }   
-    });
+   let body = document.body;
+   let toggleBtn =  document.querySelector(".toggle-round");
+   let currentTheme = localStorage.getItem('currentTheme');
+
+   
+   if (currentTheme == "active") {
+    body.classList.add('darktheme');
+    } 
+
+   toggleBtn.addEventListener('click', function () {
+
+       toggleBtn.classList.toggle('push');
+       Array.from(shadowRemove).map((shadowCut) => {
+         shadowCut.classList.toggle("shadow");
+       });
+       
+       body.classList.toggle('darktheme');
+
+       if (body.classList.contains('darktheme')) {
+           localStorage.setItem('currentTheme', 'active');
+       }else{
+        localStorage.removeItem('currentTheme');
+       }
+   });
+    
 
 $(function(){
     $(document).scroll(function(){
